3bd034219e2306e0c010256cbd9b2948b16af036
Aditya Sreevatsa K <AdityaSreevatsaK@users.noreply.github.com>
2025-05-02 19:12:25 +0530

    [minor] Added initial structure.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GetCityBikeData
+
+This service runs every hour using a GitHub Actions workflow to collect the status of CityBike stations. The collected 
+data is saved as a snapshot in a text file and automatically committed to the repository.
---
+++ get_citybike_snapshots.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import time
+from datetime import datetime
+
+import pytz
+import requests
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+logging.info("Application started successfully.")
+start_time = time.time()
+city_bike_station_information = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json'
+city_bike_station_status = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
+
+# Fetch the response
+try:
+    response = requests.get(city_bike_station_status)
+    response.raise_for_status()
+    data = response.json()
+    logging.info(f"Obtained response successfully. {data}")
+except requests.exceptions.RequestException as e:
+    logging.error(f"Failed to fetch data: {e}")
+    data = {}
+
+# Get the current date and time in New York timezone
+ny_tz = pytz.timezone("America/New_York")
+current_time = datetime.now(ny_tz)
+date_str = current_time.strftime("%Y-%m-%d")
+time_str = current_time.strftime("%H_%M")
+
+# Create a folder for the current date if it doesn't exist
+folder_name = date_str
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+# Save the response to a text file
+file_name = f"{folder_name}/{date_str}_{time_str}.txt"
+with open(file_name, "w") as file:
+    file.write(str(data))
+
+end_time = time.time()
+logging.info(f"Total time taken: {end_time - start_time}")
